1b4a679f65427a0ed9498d55d222fbe40dbe8d13
Marcus <marcusprgin121@gmail.com>
2026-05-10 07:23:11 -0400

    asd

--- sqli-sprintf.go
+++ sqli-sprintf.go
@@ -2,3 +2,4 @@ func getOrder(db *sql.DB, id string) (*Order, error) {
 	query := fmt.Sprintf("SELECT * FROM orders WHERE id = %s", id)
 	return scanOrder(db.QueryRow(query))
 }
+#lskdjfksjadhfk;jalhsdkjfa
